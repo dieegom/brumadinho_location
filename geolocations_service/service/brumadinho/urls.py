00cf769000dd729a143b2392ff95aa84e8187e1e
Cleiton Couto <dev.cleiton.couto@gmail.com>
2019-01-30 00:14:14 -0200

    Undefined name api_root in urls.py #73

--- geolocations_service/service/brumadinho/urls.py
+++ geolocations_service/service/brumadinho/urls.py
@@ -2,6 +2,7 @@ from brumadinho.views import GeolocationViewSet, VisitedLocationViewSet
 from rest_framework import renderers
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from brumadinho import views
 
 
 geolocation_list = GeolocationViewSet.as_view({
@@ -30,7 +31,7 @@ visited_location_detail = VisitedLocationViewSet.as_view({
 
 
 urlpatterns = format_suffix_patterns([
-    path('', api_root),
+    path('', views.api_root),
     path('geolocations/', geolocation_list, name="geolocation-list"),
     path('visited_locations/', visited_location_list, name="visited_location-list"),
 ])
