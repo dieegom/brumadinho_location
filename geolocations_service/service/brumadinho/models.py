a643988054d556b480f1b13e78658863ba49cfd0
Dezoito <an.dezoito@gmail.com>
2019-01-30 17:37:57 -0200

    Adds fields Radius, notes and date_visited to visited_locations models

--- geolocations_service/service/brumadinho/models.py
+++ geolocations_service/service/brumadinho/models.py
@@ -17,3 +17,10 @@ class VisitedLocation(models.Model):
 
     reference = models.CharField(max_length=100)
     location = models.ForeignKey(Geolocation, on_delete=models.PROTECT)
+    radius = models.PositiveIntegerField(
+        "Approx. radius visited from marker", blank=True, default=1)
+    date_visited = models.DateTimeField("Date of Visit", auto_now_add=True)
+    notes = models.TextField("Visit Notes",
+                            max_length=255,
+                            blank=True,
+                            null=True)
