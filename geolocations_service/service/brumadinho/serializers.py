de5201e2c4b490318e44e4f189ae96dea42c85ea
Bruno L. Carli <brunolcarli@gmail.com>
2019-02-01 01:05:40 -0200

    CHANGE geolocation em formato geoJson

--- geolocations_service/service/brumadinho/serializers.py
+++ geolocations_service/service/brumadinho/serializers.py
@@ -1,13 +1,31 @@
 # from django.contrib.auth.models import Group
 from brumadinho.models import Geolocation, VisitedLocation, FoundPeople
 from rest_framework import serializers
+from rest_framework_gis import serializers as gis_serializer
+# from django.core.serializers import serialize
+from django.contrib.gis.geos import Point
 
 
-class GeolocationSerializer(serializers.ModelSerializer):
+class GeolocationSerializer(gis_serializer.GeoFeatureModelSerializer):
+
+    # TODO corrigir para que coordinates NÃO SEJA exibido como valor de input na tela
+    # nem seja possível de ser recebido na requisição.
+
     class Meta:
         model = Geolocation
-        fields = "__all__"
+        fields = ("latitude", "longitude")
+        geo_field = "coordinates"
+        read_only_field = "coordinates"
+        write_only_fields = ("latitude", "longitude")
 
+    def create(self, data):
+        
+        data['coordinates'] = Point(
+            data.get('longitude'),
+            data.get('latitude')
+        )
+        Geolocation.objects.create(**data)
+        return data
 
 class VisitedLocationSerializer(serializers.ModelSerializer):
     class Meta:
